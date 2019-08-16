1a9bca824de583a0a1c8f748b673eeb206dbea98
anagha-01 <anaghakrishnan98@gmail.com>
2019-08-16 14:53:53 +0530

    updated

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var app=new Express()
 app.set('view engine','ejs')
 app.use(Express.static(__dirname+"/public"))
 app.get('/',(req,res)=>{       
-    res.render('index') 
+    res.render('index',
+{
+    nav:[{link:'/books',title:'Books'},{link:'/authors',title:'Authors'}],
+    title:"Library"
+}
+) 
 }
 )
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Server is running on http://localhost:3000")
-})  //port number-3000
+})  
