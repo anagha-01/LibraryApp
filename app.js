f02f1e93b6264f7e3925b3d9d6036d0c1baf9563
anagha=01 <anaghakrishnan98@gmail.com>
2019-08-17 18:08:38 +0530

    Updated

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ var book=[
         'title':'Randaamoozham',
         'author':'M T Vasudevan Nair',
         'publisher':'Cuurent Books',
-        
-    },
-    {
-        'title':'Araachar',
-        'author':'K R Meera',
-        'publisher':'D C Books',
+        'pic':"/img/mt.jpg"
         
     },
     {
@@ -30,16 +25,103 @@ var book=[
         'author':'Madhavikkutty',
         'publisher':'Curent Books',
         
+    },
+    {
+        'title':'Araachar',
+        'author':'K R Meera',
+        'publisher':'D C Books',
+        
     }
+    // {
+    //     'title':'Oru Desathinte Katha',
+    //     'author':'S K Pottakkad',
+    //     'publisher':'D C Books',
+        
+    // },
+    // {
+    //     'title':'Mayyazhippuzhayude Theerangalil',
+    //     'author':'M Mukundan',
+    //     'publisher':'D C Books',
+        
+    // },
+    // {
+    //     'title':'Chemmeen',
+    //     'author':'Thakazhi',
+    //     'publisher':'D C Books',
+        
+    // }, 
+    // {
+    //     'title':'Ini Njan Urangatte',
+    //     'author':'P K Balakrishnan',
+    //     'publisher':'D C Books',
+        
+    // },
+    // {
+    //     'title':'Aadujeevitham',
+    //     'author':'Benyamin',
+    //     'publisher':'D C Books',
+        
+    // }
 
 ]
+var aut=[
+    {
+        'name':'M T Vasudevan Nair',
+        'dob':'9 August 1933',
+        'place':'Kudallur'
+
+    },
+    {
+        'name':'O V Vijayan',
+        'dob':'2 July 1930',
+        'place':'Palakkad'
+
+    },
+    {
+        'name':'Vaikkam Muhammad Bashir',
+        'dob':'21 January 1908',
+        'place':'Thalayolapparambu'
+
+    },
+    {
+        'name':'Madhavikkutty',
+        'dob':'31 March 1934',
+        'place':'Punnayurkkulam'
+
+    },
+    {
+        'name':'K R Meera',
+        'dob':'19 February 1970',
+        'place':'Sasthamkotta'
+
+    }
+]
 app.set('view engine','ejs')
+app.get('/readmore/:id',(req,res)=>{                                                  //Read more link from books
+
+    const x=req.params.id;
+    res.render('single',{'book': book[x],
+
+    nav:[{link:'/books',title:'books'},{link:'/authors',title:'authors'}],
+
+    title:'Library'}   )
+
+})
 app.use(Express.static(__dirname+"/public"))
 app.get('/',(req,res)=>{       
     res.render('index',
 {
     nav:[{link:'/books',title:'books'},{link:'/authors',title:'authors'}],
     title:"Library"
+}
+) 
+}
+)
+app.get('/authors',(req,res)=>{       
+    res.render('authors',
+{
+    nav:[{link:'/books',title:'books'},{link:'/authors',title:'authors'}],
+    title:"Authors",aut:aut
 }
 ) 
 }
@@ -53,6 +135,16 @@ app.get('/books',(req,res)=>{
 ) 
 }
 )
+app.get('/readmore/:id',(req,res)=>{                                                     //Read more link from Authors                             
+
+    const y=req.params.id;
+    res.render('second',{'aut': aut[y],
+
+    nav:[{link:'/books',title:'books'},{link:'/authors',title:'authors'}],
+
+    title:'Library'}   )
+
+})
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Server is running on http://localhost:3000")
 })  
